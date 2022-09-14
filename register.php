7aeaae0dc5de96753c858b898fa6c461de93056e
Rosux <jcsbt2015@gmail.com>
2022-09-14 15:58:41 +0200

    small changes

--- register.php
+++ register.php
@@ -39,8 +39,37 @@
         exit();
     }
     // proceed with uploading to db then redirect to home page
-
-
+    require_once "pdo.php";
+    // create user obj
+    $userPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+    $registerUser = [
+        "name" => $userName,
+        "email" => $userEmail,
+        "password" => $userPassword
+    ];
+    // check if email is already used
+    $sql = "SELECT * FROM users WHERE email=?";
+    $p = $conn->prepare($sql);
+    $e = $p->execute([$userEmail]);
+    $result = $p->rowCount();
+    if($result > 0){
+        // deny request
+        $_SESSION['registerError'] = "Email already in use<br>";
+        header("Location: ./index.php");
+        exit();
+    }
+    // INSERT INTO `users` (`name`, `email`, `password`, `goals`, `assists`, `id`) VALUES ('name', 'email', 'password', '0', '0', NULL);
+    $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+    $p = $conn->prepare($sql);
+    $e = $p->execute($registerUser);
+    $result = $p->rowCount();
+    // check if it failed or worked
+    if($result > 0){
+        header("Location: ./home.php");
+        exit();
+    }else{
+        $_SESSION['registerError'] = "Error while registering account. try again later.<br>";
+    }
 
     
 ?>
